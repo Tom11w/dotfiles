9c91c441c50624570adb09e2237e0a6b63fa67b6
Tom11w <tom11w.tw@gmail.com>
2025-10-17 17:40:31 +1100

    nvim: plugin updates and lsp changers

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,22 +1,21 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = {
-            "ast_grep",
-            "basedpyright",
-            "bashls",
-            "clangd",
-            "eslint",
-            "lua_ls",
-            "marksman",
-            "ruff",
-            "rust_analyzer",
-            "tailwindcss",
-            "ts_ls",
-        },
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "ast_grep",
+      "bashls",
+      "clangd",
+      "eslint",
+      "lua_ls",
+      "marksman",
+      "ruff",
+      "rust_analyzer",
+      "tailwindcss",
+      "ts_ls",
     },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
 }
